8a792be490b6574a3c365b87e6da8e7776a91c98
kxwzhang <kxwzhang@gmail.com>
2021-10-19 14:40:49 -0700

    Update component to only handle entering drawing state

--- src/components/CreateShape.js
+++ src/components/CreateShape.js
@@ -1,44 +1,23 @@
-import React, { useState } from 'react'
-import { getElOffset, getCoords, mapShape } from '../utils/ShaperHelpers'
-import { Circle, Rectangle, Line } from '../components/Shapes'
+import React from 'react'
 
 const CreateShape = ({ 
   shape, 
   cursorState,
   setCursorState,
-  drawings,
-  setDrawings,
 }) => {
-  /* Handle mouse down */
-  const mouseDown = (e) => {
-    let mouseCoords = getCoords(e)
-    let offset = getElOffset(e.target)
-    console.log('offset', offset)
-    const startX = mouseCoords.x - offset.left
-    const startY = mouseCoords.y - offset.top
-    setCursorState({ 
-      ...cursorState, 
-      width: 5, 
-      height: 5, 
-      offsetX: offset.left, 
-      offsetY: offset.top,
-      startX: startX, 
-      startY: startY, 
-      previousX:startX, 
-      previousY:startY, 
-      isDrawing: true, 
-    })
-  }
-
   const handleCreateShape = (e) => {
-    console.log('Creating shape...')
-    setCursorState({ ...cursorState, isDrawing: true })
-    mouseDown(e)
+    console.log('Entering drawing state...')
+    setCursorState({ ...cursorState, isDrawing: true, type: shape })
   }
 
   return (
     <div>
-      <button onMouseDown={(e) => handleCreateShape(e)}>{mapShape(shape)}</button>
+      <button 
+        onClick={(e) => handleCreateShape(e)}
+        className="ShapeButton"
+      >
+        {shape}
+      </button>
     </div>
   )
 }
