bf4a2236e5bccdd5db443b4a343d578164549457
kxwzhang <kxwzhang@gmail.com>
2021-10-19 21:49:09 -0700

    Render new dynamic shape componenet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import React, { useState } from 'react'
+import * as R from 'ramda'
 import CreateShape from './components/CreateShape'
+import DeleteShape from './components/DeleteShape';
 import { mapShape, mouseDown } from './utils/ShaperHelpers'
+import { Shape } from './components/Shapes'
 
 function App() {
   /* States */
@@ -9,7 +12,9 @@ function App() {
     startX: 0,
     startY: 0,
     isDrawing: false,
+    isDeleting: false,
     type: null,
+    shapeId: null,
   }
   const [cursorState, setCursorState] = useState(initialCursorState)
   const [drawings, setDrawings] = useState([]) /* List of all shapes that are drawn */
@@ -24,8 +29,21 @@ function App() {
       />
     )
   }
+
+  const DeleteButton = () => {
+    return (
+      <DeleteShape 
+        cursorState={cursorState}
+        setCursorState={setCursorState}
+        drawings={drawings}
+        setDrawings={setDrawings}
+      />
+    )
+  }
   
   const shapes = ['circle', 'rectangle', 'line']
+
+  console.log('drawings', drawings)
 
   return (
     <div 
@@ -36,16 +54,24 @@ function App() {
         <div>Welcome to ArtFart</div>
         <div className="Controls">
           {shapes.map(shape => <ShapeButton shape={shape} />)}
+          <DeleteButton />
         </div>
       </div>
       <svg width="100%" height="100vh" className="Canvas">
-        {drawings.length > 0 &&
-        drawings.map((drawing, index) => {
-          const Shape = mapShape(drawing, index)
+        {!R.isEmpty(drawings) &&
+        R.map((drawing, index) => {
+          {/* const Shape = mapShape(drawing, cursorState, drawings, setDrawings, index) */}
           return (
-            Shape
+            <Shape 
+              key={index} 
+              drawing={drawing} 
+              drawings={drawings} 
+              setDrawings={setDrawings} 
+              cursorState={cursorState} 
+              setCursorState={setCursorState}
+            />
           )
-        })}
+        })(drawings)}
       </svg>
     </div>
   )
