265556231276d360af99413b3022af5c447d5007
kxwzhang <kxwzhang@gmail.com>
2021-10-19 21:48:59 -0700

    Create a dynamic shape component

--- src/components/Shapes.js
+++ src/components/Shapes.js
@@ -1,22 +1,81 @@
 import React from 'react'
+import * as R from 'ramda'
 
-export const Circle = ({ props }) => {
-  const { startX, startY } = props
+const handleAction = (e, cursorState, setCursorState, drawings, setDrawings, shapeId) => {
+  if (cursorState.isDeleting) {
+    console.log('State is in deleting...')
+    /* Grabs index of shape that is to be deleted */
+    const drawingIndex = R.findIndex(R.propEq('shapeId', shapeId))(drawings)
+    /* Slice it out of the drawings state */
+    const drawingsAfterDeletion = R.concat(
+      R.slice(0, drawingIndex, [...drawings]), 
+      R.slice(drawingIndex + 1, drawings.length,[...drawings])
+    )
+    setDrawings(drawingsAfterDeletion)
+    setCursorState({ ...cursorState, isDeleting: false })
+  }
+}
+
+export const Circle = ({ cursorState, drawings, setDrawings, props }) => {
+  const { startX, startY, shapeId } = props
   return (
-      <circle cx={startX} cy={startY} r="50" />
+    <circle cx={startX} cy={startY} r="50" onClick={e => handleAction(e, cursorState, drawings, setDrawings, shapeId)} />
   )
 }
 
-export const Rectangle = ({ props }) => {
-  const { startX, startY } = props
+export const Rectangle = ({ cursorState, drawings, setDrawings, props }) => {
+  const { startX, startY, shapeId } = props
   return (
-      <rect x={startX} y={startY} width="30" height="30" />
+    <rect x={startX} y={startY} width="30" height="30" onClick={e => handleAction(e, cursorState, drawings, setDrawings, shapeId)} />
   )
 }
 
-export const Line = ({ props }) => {
-  const { startX, startY } = props
+export const Line = ({ cursorState, drawings, setDrawings, props }) => {
+  const { startX, startY, shapeId } = props
   return (
-    <line x1={startX} x2={"50"} y1={startY} y2="150" stroke="black" strokeWidth="5" />
+    <line x1={startX} x2={"50"} y1={startY} y2="150" stroke="black" strokeWidth="5" onClick={e => handleAction(e, cursorState, drawings, setDrawings, shapeId)} />
   )
+}
+
+/* Dynamic Component for defining shapes */
+export const Shape = ({ drawing, cursorState, setCursorState, drawings, setDrawings }) => {
+  console.log('cursorState', cursorState)
+  const { type, startX, startY, shapeId } = drawing
+  const props = [cursorState, setCursorState, drawings, setDrawings, shapeId]
+
+  switch (type) {
+    case 'circle':
+      return (
+        <circle 
+          cx={startX} 
+          cy={startY} 
+          r="50" 
+          onClick={e => handleAction(e, ...props)} 
+        />
+      )
+    case 'rectangle':
+      return (
+        <rect 
+          x={startX} 
+          y={startY} 
+          width="30" 
+          height="30" 
+          onClick={e => handleAction(e, ...props)} 
+        />
+      )
+    case 'line':
+      return (
+        <line 
+          x1={startX} 
+          x2={"50"} 
+          y1={startY} 
+          y2="150" 
+          stroke="black" 
+          strokeWidth="5" 
+          onClick={e => handleAction(e, ...props)} 
+        />
+      )
+    default:
+      break
+  }
 }
